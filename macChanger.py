b5e48ad2c366ddde308fae5f3ed2f4b8464df33f
Büşra Yıldız <402515@ogr.ktu.edu.tr>
2024-06-24 08:30:52 -0400

    Add files via upload

---
+++ macChanger.py
@@ -0,0 +1,35 @@
+import subprocess
+import optparse
+import re
+
+
+def getUserInput():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i", "--interface", dest="interface", help="interface to change")
+    parse_object.add_option("-m", "--mac", dest="macAddress", help="new MAC address")
+    return parse_object.parse_args()
+
+def changeMAC(interface, macAddress):
+    subprocess.run(["sudo", "ifconfig", interface, "down"])
+    subprocess.run(["sudo", "ifconfig", interface, "hw", "ether", macAddress])
+    subprocess.run(["sudo", "ifconfig", interface, "up"])
+
+def control(interface):
+    output = subprocess.check_output(["ifconfig", interface])
+    newMAC = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(output))
+    if newMAC:
+        return newMAC.group(0)
+    else: 
+        return None
+
+
+print("----------MAC CHANGER----------")
+(userInput, arguments) = getUserInput()
+changeMAC(userInput.interface, userInput.macAddress)
+finalMAC = control(userInput.interface)
+
+if finalMAC == userInput.macAddress:
+    print("MAC address successfully changed!")
+else:
+    print("Error changing MAC address!")
+
